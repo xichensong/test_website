52037d39b87248decde29c19616319e0a0db273a
xichen_song <xichen_song@berkeley.edu>
2026-07-06 00:09:39 -0400

    add download query to blob url

--- src/App.jsx
+++ src/App.jsx
@@ -71,8 +71,7 @@ function BananaDisplay({ progress }) {
 }
 
 function WineModel() {
-  const { scene } = useGLTF('https://yvulkalxiqfoecqr.public.blob.vercel-storage.com/wine_bottles_01_4k.glb')
-
+  const { scene } = useGLTF('https://yvulkalxiqfoecqr.public.blob.vercel-storage.com/wine_bottles_01_4k.glb?download=1')
   return (
     <group rotation={[0, 0, 0]} scale={5.2}>
       <Center>
