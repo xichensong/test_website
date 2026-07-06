c25e39e16f92bdc98693b1a3ccc856662f87a733
xichen_song <xichen_song@berkeley.edu>
2026-07-05 23:50:56 -0400

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -71,7 +71,7 @@ function BananaDisplay({ progress }) {
 }
 
 function WineModel() {
-  const { scene } = useGLTF('/models/wine_bottles_01_4k.glb')
+  const { scene } = useGLTF('https://3ajq5pyzgkfuyqdn.public.blob.vercel-storage.com/wine_bottles_01_4k.glb')
 
   return (
     <group rotation={[0, 0, 0]} scale={5.2}>
